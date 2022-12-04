c147a031c3972db29bbf5adb0cc000fe8777b6d1
Andy Kaminski <andy.kaminski42@gmail.com>
2022-12-04 16:32:50 -0500

    Added responsiveness to header

--- react-portfolio/src/components/header/Header.js
+++ react-portfolio/src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css'
 import Links from './Links';
-import Profile from '../../assets/profile-pic.JPG'
+import Profile from '../../assets/profile-pic.png'
 import SocialMediaIcons from './SocialMediaIcons';
 
 const Header = () => {
@@ -12,10 +12,10 @@ const Header = () => {
         <h1>Andrew Kaminski</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <Links />
-
         <div className='profile-pic'>
           <img src={Profile} alt='profile picture of Andrew Kaminski' />
         </div>
+
         <SocialMediaIcons />
       </div>
     </header>
--- react-portfolio/src/components/header/SocialMediaIcons.js
+++ react-portfolio/src/components/header/SocialMediaIcons.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {FaLinkedin, FaGithub} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
 
 const SocialMediaIcons = () => {
   return (
     <div className='social-icons'>
       <a href='https://linkedin.com/in/andrewkaminski214' target='_blank'>
-        <FaLinkedin />
+        <IconContext.Provider value={{ className: 'shared-class', size: 32 }}>
+          <>
+            <FaLinkedin />
+          </>
+        </IconContext.Provider>
       </a>
       <a href='https://github.com/AndrewKamSki' target='_blank'>
-        <FaGithub />
+        <IconContext.Provider value={{ className: 'shared-class', size: 32 }}>
+          <>
+            <FaGithub />
+          </>
+        </IconContext.Provider>
       </a>
     </div>
   )
